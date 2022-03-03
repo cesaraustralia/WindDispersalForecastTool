d0fcb394bbf88f839a21760c027dcec3ad345bcc
Roozbeh Valavi <valavi.r@gmail.com>
2022-03-03 13:40:24 +1100

    download funciton

--- Scripts/plotting.R
+++ Scripts/plotting.R
@@ -11,14 +11,15 @@ con <- DBI::dbConnect(
   dbname = Sys.getenv("POSTGRES_DB")
 )
 au <- sf::st_read(con, layer = "aus_border")
+DBI::dbDisconnect(con)
 
 fl <- list.files("C://Users/61423/Desktop/", pattern = "^gfs.t12z.pgrb2")
 
 r <- terra::rast(paste0("C://Users/61423/Desktop/", fl))
 r
 
-plot(r[[1]])
+plot(r[[4]], col = viridis::viridis(30, option = "E", direction = 1))
 plot(sf::st_geometry(au), add = TRUE)
 
 setMinMax(r)
-terra::animate(r)
+terra::animate(r, n = 3)
