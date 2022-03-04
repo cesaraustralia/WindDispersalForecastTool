3a6dc934a7313ca233dd8f009e86dcbc812229d1
Roozbeh Valavi <valavi.r@gmail.com>
2022-03-04 15:09:12 +1100

    fixing plot and wind speed

--- Scripts/plotting.R
+++ Scripts/plotting.R
@@ -30,10 +30,12 @@ plot(st_geometry(au), add = TRUE)
 
 
 # read U and V component for wind direction
-u_ms <- rast("C://Users/61423/Desktop/GFStest/gfs_ugrd_20220302_t00z_f001")
-v_ms <- rast("C://Users/61423/Desktop/GFStest/gfs_vgrd_20220302_t00z_f001")
-# calculate wind direction
+u_ms <- rast("C://Users/61423/Desktop/GFStest/gfs_ugrd_20220228_t18z_f000")
+v_ms <- rast("C://Users/61423/Desktop/GFStest/gfs_vgrd_20220228_t18z_f000")
+# calculate wind speed
 wind_abs <- sqrt(u_ms^2 + v_ms^2)
+plot(wind_abs)
+# calculate wind direction
 wind_dir_trig_to <- atan2(u_ms/wind_abs, v_ms/wind_abs) 
 wind_dir_trig_to_degrees <- wind_dir_trig_to * 180/pi
 
@@ -41,11 +43,11 @@ plot(wind_dir_trig_to_degrees)
 
 ## aggregate the data or reduce the arrow in plotting
 # wind_dir_agg <- terra::aggregate(wind_dir_trig_to_degrees, fact=5)
+# plot(wind_dir_agg)
+# wind_speed_agg <- terra::aggregate(wind_abs, fact=5)
+# plot(wind_speed_agg)
 wind_dir_agg <- wind_dir_trig_to_degrees
-plot(wind_dir_agg)
-# wind_speed_agg <- terra::aggregate(v_ms, fact=5)
-wind_speed_agg <- v_ms
-plot(wind_speed_agg)
+wind_speed_agg <- wind_abs
 
 wind_dt <- as.data.frame(wind_speed_agg, xy = TRUE)
 names(wind_dt) <- c("Lon", "Lat", "mean_wind")
@@ -62,7 +64,7 @@ ggplot(wind_dt,
            y = Lat, 
            fill = mean_wind, 
            angle = wind_dir[c(TRUE, rep(NA, 100))], # causes some values not to plot
-           radius = scales::rescale(mean_wind, c(0.2, 1.20)))) +
+           radius = scales::rescale(mean_wind, c(0.5, 2)))) +
   geom_raster() +
   geom_spoke(arrow = grid::arrow(length = unit(0.05, 'inches'))) + 
   geom_sf(data = au, fill = NA, inherit.aes = FALSE) +
