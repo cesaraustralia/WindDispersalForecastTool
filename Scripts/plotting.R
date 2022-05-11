18e035e88e0e1f4277bece3fa3eddfb4be697f26
Roozbeh Valavi <valavi.r@gmail.com>
2022-05-11 16:27:16 +1000

    reading multiple files

--- Scripts/plotting.R
+++ Scripts/plotting.R
@@ -7,13 +7,13 @@ library(RColorBrewer)
 
 con <- DBI::dbConnect(
   RPostgres::Postgres(),
-  host = Sys.getenv("POSTGRES_HOST"),
+  host = "localhost",
   port = 5432,
-  user = Sys.getenv("POSTGRES_USER"),
-  password = Sys.getenv("POSTGRES_PASSWORD"),
-  dbname = Sys.getenv("POSTGRES_DB")
+  user = "admin",
+  password = "cesarau",
+  dbname = "postgres"
 )
-au <- sf::st_read(con, layer = "aus_border")
+au <- sf::st_read(con, layer = "aus_states")
 DBI::dbDisconnect(con)
 
 
@@ -34,46 +34,93 @@ plot(st_geometry(au), add = TRUE)
 # read U and V component for wind direction
 u_ms <- rast("C://Users/61423/Desktop/GFStest/gfs_ugrd_20220226_t00z_f000")
 v_ms <- rast("C://Users/61423/Desktop/GFStest/gfs_vgrd_20220226_t00z_f000")
+plot(c(u_ms, v_ms))
 # calculate wind speed
 wind_abs <- sqrt(u_ms^2 + v_ms^2)
 plot(wind_abs)
 # calculate wind direction
-wind_dir_trig_to <- atan2(u_ms/wind_abs, v_ms/wind_abs) 
+wind_dir_trig_to <- atan2(u_ms/wind_abs, v_ms/wind_abs)
 wind_dir_trig_to_degrees <- wind_dir_trig_to * 180/pi
-
 plot(wind_dir_trig_to_degrees)
+
+
 
 ## aggregate the data or reduce the arrow in plotting
 # wind_dir_agg <- terra::aggregate(wind_dir_trig_to_degrees, fact=5)
 # plot(wind_dir_agg)
 # wind_speed_agg <- terra::aggregate(wind_abs, fact=5)
 # plot(wind_speed_agg)
+# max 134°45'5"E 8°27'20"S
+# min 119°57'E 14°31'4"S
 wind_dir_agg <- wind_dir_trig_to_degrees
 wind_speed_agg <- wind_abs
 
 wind_dt <- as.data.frame(wind_speed_agg, xy = TRUE)
 names(wind_dt) <- c("Lon", "Lat", "mean_wind")
-windir <- wind_dir_agg %>% 
-  as.data.frame() %>% 
+windir <- wind_dir_agg %>%
+  as.data.frame() %>%
   setNames("wind_dir")
 names(windir)
 wind_dt <- cbind(wind_dt, windir)
 head(wind_dt)
 
 # plot the wind directoin
-ggplot(wind_dt, 
-       aes(x = Lon , 
-           y = Lat, 
-           fill = mean_wind, 
+ggplot(wind_dt,
+       aes(x = Lon ,
+           y = Lat,
+           fill = mean_wind,
            angle = wind_dir[c(TRUE, rep(NA, 55))], # causes some values not to plot
            radius = scales::rescale(mean_wind, c(0.5, 2)))) +
   geom_raster() +
-  geom_spoke(arrow = grid::arrow(length = unit(0.05, 'inches'))) + 
+  geom_spoke(arrow = grid::arrow(length = unit(0.05, 'inches'))) +
   geom_sf(data = au, fill = NA, inherit.aes = FALSE) +
-  scale_fill_distiller(palette = "RdYlGn") + 
-  # coord_equal(expand = 0) + 
+  scale_fill_distiller(palette = "RdYlGn") +
+  # coord_equal(expand = 0) +
   theme_minimal() +
-  theme(legend.position = 'bottom', 
+  theme(legend.position = 'bottom',
+        legend.direction = 'horizontal') +
+  labs(fill = "Wind Speed", x = "Longitude", y = "Latitude")
+
+
+
+
+
+
+exx <- c(120, 134.75, -14.5, -8.5)
+wind_dir_agg <- wind_dir_trig_to_degrees %>%
+  terra::crop(exx)
+wind_speed_agg <- wind_abs %>%
+  terra::crop(exx)
+
+# create colour palette and plot
+mycol <- colorRampPalette(rev(RColorBrewer::brewer.pal(n = 11, name = "RdYlGn")))
+c(wind_dir_agg, wind_speed_agg) %>%
+  raster::stack() %>%
+  plot(col = mycol(50))
+
+wind_dt <- as.data.frame(wind_speed_agg, xy = TRUE)
+names(wind_dt) <- c("Lon", "Lat", "mean_wind")
+windir <- wind_dir_agg %>%
+  as.data.frame() %>%
+  setNames("wind_dir")
+names(windir)
+wind_dt <- cbind(wind_dt, windir)
+head(wind_dt)
+
+# plot the wind directoin
+ggplot(wind_dt,
+       aes(x = Lon ,
+           y = Lat,
+           fill = mean_wind,
+           angle = wind_dir,#[c(TRUE, rep(NA, 5))], # causes some values not to plot
+           radius = scales::rescale(mean_wind, c(0.05, 0.5)))) +
+  geom_raster() +
+  geom_spoke(arrow = grid::arrow(length = unit(0.05, 'inches'))) +
+  # geom_sf(data = au, fill = NA, inherit.aes = FALSE) +
+  scale_fill_distiller(palette = "RdYlGn") +
+  # coord_equal(expand = 0) +
+  theme_minimal() +
+  theme(legend.position = 'bottom',
         legend.direction = 'horizontal') +
   labs(fill = "Wind Speed", x = "Longitude", y = "Latitude")
 
